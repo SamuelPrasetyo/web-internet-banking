d88c10a28a1c90aa7c5827ed0f74744007c0f525
Samuel Prasetyo <sprasetyo0511@gmail.com>
2023-10-04 09:16:59 +0700

    Update Tampilan Form Login dari Branch Frontend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,30 +4,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <title>Login & Registration Form</title>
+
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="card">
-        <form action="{{ url('api/login') }}" method="post">
-            <label for="email">Username</label>
-            <input type="text" name="email" id="email">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
+    <div class="container">
 
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
+
+
+        <div class="login form">
+            <header>Login</header>
+            <form action="{{ url('api/login') }}" method="post">
+                <input type="email" name="email" placeholder="Enter your email">
+                <input type="password" name="password" placeholder="Enter your password">
+                <a href="#">Forgot password?</a>
+                <input type="submit" class="button" value="Login">
+            </form>
+            <div class="signup">
+                <span class="signup">Don't have an account?
+                    <label for="check">Signup</label>
+                </span>
+            </div>
+        </div>
+
+
+
+        <div class="registration form">
+            <header>Signup</header>
+            <form action="#">
+                <input type="text" placeholder="Enter your email">
+                <input type="password" placeholder="Create a password">
+                <input type="password" placeholder="Confirm your password">
+                <input type="button" class="button" value="Signup">
+            </form>
+            <div class="signup">
+                <span class="signup">Already have an account?
+                    <label for="check">Login</label>
+                </span>
+            </div>
+        </div>
+
+
+
     </div>
-
-    <br>
-
-    <h3>sp123888</h3>
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
